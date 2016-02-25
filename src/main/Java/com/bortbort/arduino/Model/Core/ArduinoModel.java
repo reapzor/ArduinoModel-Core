ab145bfe184bf7f92d1fc02e159a95ed7ea656c8
reapzor <reapzor@gmail.com>
2016-02-25 09:45:16 -0500

    .

---
+++ src/main/Java/com/bortbort/arduino/Model/Core/ArduinoModel.java
@@ -0,0 +1,33 @@
+package com.bortbort.arduino.Model.Core;
+
+import com.bortbort.arduino.FiloFirmata.Firmata;
+import com.bortbort.arduino.FiloFirmata.FirmataConfiguration;
+import com.bortbort.arduino.FiloFirmata.MessageListener;
+import com.bortbort.arduino.FiloFirmata.Messages.SysexCapabilityMessage;
+import com.bortbort.arduino.FiloFirmata.Messages.SysexCapabilityQueryMessage;
+
+/**
+ * Created by chuck on 2/24/2016.
+ */
+public class ArduinoModel {
+    Firmata firmataClient;
+
+    public ArduinoModel(FirmataConfiguration firmataConfiguration) {
+        firmataClient = new Firmata(firmataConfiguration);
+    }
+
+    private void discoverDevice() {
+        firmataClient.addMessageListener(new MessageListener<SysexCapabilityMessage>() {
+            @Override
+            public void messageReceived(SysexCapabilityMessage message) {
+                //message.
+            }
+        });
+        firmataClient.sendMessage(new SysexCapabilityQueryMessage());
+    }
+
+
+    public Firmata getFirmataClient() {
+        return firmataClient;
+    }
+}
